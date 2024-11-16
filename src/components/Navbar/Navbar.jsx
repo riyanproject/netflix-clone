fa8f9fc6f5555b7bbceef26f63159c33e0a04730
riyan ahemed khan <riyanahemedkhangmail.com>
2024-11-16 11:44:39 +0530

    Sign up and sign in page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,10 +26,13 @@ const Navbar = () => {
         <img src={search_icon} alt="" className='icons'/>
         <p>Children</p>
         <img src={bell_icon} alt="" className='icons'/>
-        <div className="navbar-profile"></div>
+        <div className="navbar-profile">
         <img src={profile_img} alt="" className='profile'/>
         <img src={caret_icon} alt="" />
-
+        <div className="dropdown">
+          <p>Sign out of Netflix</p>
+        </div>
+        </div>
       </div>
     </div>
   )
